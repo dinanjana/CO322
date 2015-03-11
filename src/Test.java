62664a577548aaa3768d8982e51c545342abac3b
Dinanjana Gunaratne <dinanjanagunaratne@gmail.com>
2015-03-11 20:53:22 +0530

    1st Patch
    Change HEap Interface

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dinanajana
+ */
+public class Test {
+    
+    static int a = 0;
+    
+    public static void main (String arg[]){
+    
+        System.out.println(a++);
+        
+        main(arg);
+    
+    }
+}
